ff01df895325e0b958b352343940c447eb2bf813
MathurinChrist <121124199+MathurinChrist@users.noreply.github.com>
2025-08-11 15:09:04 +0200

    Update articles.schema.js
    update article modek

--- api/articles/articles.schema.js
+++ api/articles/articles.schema.js
@@ -1,33 +1,18 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
 
-const articleSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: [true, "Le titre est obligatoire"],
-      trim: true,
-      minlength: [3, "Le titre doit contenir au moins 3 caractères"],
-    },
-    content: {
-      type: String,
-      required: [true, "Le contenu est obligatoire"],
-      minlength: [10, "Le contenu doit contenir au moins 10 caractères"],
-    },
-    status: {
-      type: String,
-      enum: ["draft", "published"],
-      default: "draft",
-      required: true,
-    },
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+const articleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { 
+    type: String, 
+    required: true, 
+    minlength: [5, "Le contenu doit contenir au moins 10 caractères"] 
   },
-  {
-    timestamps: true, // timestamp pour les propriété createdAt et updatedAt de manière automatique
-  }
-);
+  status: {
+    type: String,
+    enum: ["draft", "published"],
+    default: "draft",
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+}, { timestamps: true });
 
-module.exports = model("Article", articleSchema);
+module.exports = mongoose.model("Article", articleSchema);
