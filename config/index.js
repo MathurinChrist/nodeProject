33e78c3cf5c312f2f71aafa3d91d34239bfc30c4
MathurinChrist <121124199+MathurinChrist@users.noreply.github.com>
2025-08-11 15:13:56 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongoUri: "mongodb://localhost:27017/myapp",
+  mongoUriTest: "mongodb://localhost:27017/myapp_test",
   secretJwtToken: "test",
 };
